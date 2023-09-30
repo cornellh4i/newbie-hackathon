63410a1fc0e4df02b0ea3f64fa693c5e119e7d38
leaneying <leaneying23@gmail.com>
2023-09-30 18:10:35 -0400

    finished PostCard.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,10 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <PostCard />
+      <PostCard username="Username" title="Lab 5 Question" question="for the 
+      layout 2 of lab 5, is it okay if the orange <hi> element ( with text 
+      'Layout2') changes width as you change the screen size? Or does it have 
+      to be fixed as shown in the video?" course="CS1110" rank={5} comments={["hi", "hi"]} />
     </div>
   );
 }
--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -1,48 +1,57 @@
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
+import Card from '@mui/material/Card'; import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import { Avatar, Box, Button, SvgIcon, TextField, Toolbar } from '@mui/material';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import { Avatar, Box, } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import { blue } from '@mui/material/colors';
 import CommentIcon from '@mui/icons-material/Comment';
+import CardHeader from '@mui/material/CardHeader';
 
-export default function PostCard() {
 
-
+const PostCard = ({ username, title, question, course, rank, comments }:
+  { username: string, title: string, question: string, course: string, rank: number, comments: string[] }) => {
   return (
     <Box display="flex" sx={{ justifyContent: "center" }} border={10} borderColor="transparent">
       <Card sx={{ maxWidth: 800 }}>
+
         <CardContent>
           {
             <Avatar sx={{ bgcolor: blue[500] }} aria-label="user"></Avatar>
           }
           <Box display="flex" sx={{ justifyContent: "left" }} borderColor="transparent" paddingTop={1}>
-            Username r/CS1110
+            {username} r/{course}
           </Box>
           <CardActions sx={{ justifyContent: "right" }}>
-            <IconButton aria-label="upvote">
+            <IconButton aria-label="votes">
               <ArrowUpwardIcon />
-            </IconButton>
-            <IconButton aria-label="downvote">
+              <Box paddingLeft={1} paddingRight={1} >
+                <Typography color="black">
+                  {rank}
+                </Typography>
+              </Box>
               <ArrowDownwardIcon />
             </IconButton>
           </CardActions>
-          <Typography variant="body1">
-            Lab 5 Question
-
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            For the layout 2 of lab 5, is it okay if the orange h1 element changes
-            width as you change the screen size? Or does it have to be fixed as
-            shown in the video?
-          </Typography>
+          <Box display="flex" paddingLeft={2.5} paddingBottom={0.5}>
+            <Typography variant="body1" align='left'>
+              {title}
+            </Typography>
+          </Box>
+          <Box display="flex" paddingLeft={2.5}>
+            <Typography variant="body2" color="text.secondary" align='left'>
+              {question}
+            </Typography>
+          </Box>
           <CardActions sx={{ justifyContent: "left" }}>
             <IconButton aria-label="comment">
               <CommentIcon />
+              <Box paddingLeft={1} paddingRight={1} >
+                <Typography >
+                  {comments.length} comments
+                </Typography>
+              </Box>
             </IconButton>
           </CardActions>
         </CardContent>
@@ -52,3 +61,4 @@ export default function PostCard() {
     </Box>
   );
 }
+export default PostCard
