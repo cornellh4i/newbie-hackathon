0c66714053a86d487eaef6956e8b0d3395b368d2
Diego Marques <dlm352@cornell.edu>
2023-09-30 18:32:33 -0400

    dlm352/created frontend create post button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import PostButton from './components/PostButton';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <PostButton />
+
     </div>
   );
 }
